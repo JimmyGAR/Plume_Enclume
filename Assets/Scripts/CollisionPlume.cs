05f2f0301cfc0021ddc8c9e2e969ffaa4177c083
Zeineb2727 <zeinebcosgun9@gmail.com>
2025-05-24 17:01:13 +0200

    Fin de ma scene + pics

--- Assets/Scripts/CollisionPlume.cs
+++ Assets/Scripts/CollisionPlume.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionPlume : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public int life = 3;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Spike"))
+        {
+            if (collision.CompareTag("Spike"))
+            {
+                TakeDamage(3);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int damage)
     {
-        
+        life -= damage;
+        Die();
+    }
+
+    public void Die()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector3.up * 150);
+        GetComponent<Collider2D>().isTrigger = true;
+        Invoke("RestartLevel", 1);
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
     }
 }
+ 
