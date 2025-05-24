7691975996efeb9dacf5665cd71e4acb172e91ad
JimmyGAR <jimmy.garnier1110@gmail.com>
2025-05-24 10:07:49 +0200

    Amelioration du menu avec une animation et semi fonctionnel (en cours)

--- Assets/Scripts/StartScene.cs
+++ Assets/Scripts/StartScene.cs
@@ -1,10 +1,48 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System.Collections;
+using Unity.VisualScripting;
 
 public class StartScene : MonoBehaviour
 {
+    Animator animator;
+    public SpriteRenderer forgeron;
+    public SpriteRenderer logo;
+    public Canvas myCanvasStart;
+    public Canvas myCanvasMenu;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        myCanvasMenu.gameObject.SetActive(false);
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void PlayAnimationStart()
+    {
+        animator.SetBool("Start", true);
+        myCanvasStart.gameObject.SetActive(false);
+
+        StartCoroutine(WaitAndDoSomething());
+    }
+
+    IEnumerator WaitAndDoSomething()
+    {
+        yield return new WaitForSeconds(1f);
+
+        Destroy(forgeron.gameObject);
+        Destroy(logo.gameObject);
+
+        myCanvasMenu.gameObject.SetActive(true);
     }
 }
